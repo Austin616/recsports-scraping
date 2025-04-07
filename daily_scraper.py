f2a544126cac5ee72be02d5a66cb50977f7c7c82
Austin <austintran616@gmail.com>
2025-04-07 15:26:38 -0500

    temp

---
+++ daily_scraper.py
@@ -0,0 +1,15 @@
+import requests
+
+def run_scrape():
+    try:
+        response = requests.post("https://gym-api.onrender.com/scrape")
+        if response.status_code == 200:
+            print("Scraping successful!")
+        else:
+            print(f"Scraping failed with status code: {response.status_code}")
+            print(response.text)
+    except Exception as e:
+        print(f"An error occurred while scraping: {e}")
+
+if __name__ == "__main__":
+    run_scrape()
