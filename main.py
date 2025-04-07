ad723c7d83b7b88519884dd0408f5004a6e10387
Austin Tran <austintran616@gmail.com>
2025-04-07 12:38:17 -0500

    gunicorn

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from scraper import scrape_all_facilities
 import os
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+# Shared data store
+facilities_data = {}
+facilities_data = scrape_all_facilities()
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"message": "Welcome to the UT RecSports Scraper API!"})
+
+@app.route('/facilities', methods=['GET'])
+def get_facilities():
+    return jsonify(facilities_data)
+
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    global facilities_data
+    facilities_data = scrape_all_facilities()
+    return jsonify(facilities_data)
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # No need to run app here since Gunicorn will handle it
+    pass
